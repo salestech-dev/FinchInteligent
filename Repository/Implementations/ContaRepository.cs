60f7821cfe659d1a1f3dcaaa07a26ca1b555ef4b
salestech-dev <lcpinhosales@gmail.com>
2026-01-21 21:17:33 -0300

    Criação do implementations para acesso ao banco de dados

---
+++ Repository/Implementations/ContaRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace finchInteligent.Repository.Implementations
+{
+    public class ContaRepository
+    {
+        
+    }
+}
---
+++ Repository/Implementations/UsuarioRepository.cs
@@ -0,0 +1,53 @@
+using finchInteligent.Data;
+using finchInteligent.Models;
+using finchInteligent.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+public class UsuarioRepository : IUsuarioRepository
+{
+    private readonly AppDbContext _context;
+
+    public UsuarioRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Usuario> CreateAsync(Usuario usuario)
+    {
+        await _context.Usuarios.AddAsync(usuario);
+        await _context.SaveChangesAsync();
+        return usuario;
+    }
+
+    public async Task<Usuario?> GetByIdAsync(int id)
+    {
+        return await _context.Usuarios.FindAsync(id);
+    }
+
+    public async Task<Usuario?> GetByEmailAsync(string email)
+    {
+        return await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task<bool> ExistsAsync(int id)
+    {
+        return await _context.Usuarios.AnyAsync(u => u.Id == id);
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var usuario = await _context.Usuarios.FindAsync(id);
+        if (usuario == null) return false;
+
+        _context.Usuarios.Remove(usuario);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<Usuario> UpdateAsync(Usuario usuario)
+    {
+        _context.Usuarios.Update(usuario);
+        await _context.SaveChangesAsync();
+        return usuario;
+    }
+}
