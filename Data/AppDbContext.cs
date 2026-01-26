4556a98de318f0c461aae7ab4faea008ccae0110
salestech-dev <lcpinhosales@gmail.com>
2026-01-26 12:32:42 -0300

    Mudança na herança da classe DbContext para IdentotyDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using finchInteligent.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace finchInteligent.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<Usuario>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
